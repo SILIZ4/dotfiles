814d400b45b013b12a718377fc02000fd71e69c8
Simon Lizotte <simon.lizotte.1@ulaval.ca>
2023-04-05 14:46:04 -0400

    show signature instead of hover

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -29,7 +29,7 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("n", "<leader>c", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>i", function() vim.lsp.buf.signature_help() end, opts)
 end)
 vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end)
 
